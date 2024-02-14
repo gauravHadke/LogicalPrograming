7c818ecadbdfecd98b78ec7a41d06105d7e522e9
gauravHadke <101452481+gauravHadke@users.noreply.github.com>
2024-02-14 14:50:11 +0530

    Update EmployeeTest1.java

--- EmployeeTest1.java
+++ EmployeeTest1.java
@@ -5,7 +5,7 @@ public class EmployeeTest1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//add lines,,,,,,,,,,,
-		
+		System.out.print("Blank body");
 	}
 
 }
